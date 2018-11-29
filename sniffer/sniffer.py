bd09781e476342ea55a6626efecc1ea925dd3434
Guille <cartogreen@gmail.com>
2018-11-29 17:38:01 +0100

    sniffer v0.1

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -1,21 +1,50 @@
 #!/usr/bin/python
 import scapy.all as scapy
-import scapy_http.http
 import socket
+import json
 
-def expand(packet):
+# Enumeration of the packet's layers
+def enumeration(packet):
 	yield packet.name
 	while packet.payload:
 		packet =packet.payload
 		yield packet.name
 
-def printer(packet):
-	for i in expand(packet):
+# # Mapping between the protocol number and its name
+# def protocol(number):
+# 	table = {num:name[8:] for name,num in vars(socket).items() if name.startswith("IPPROTO")}
+# 	return table[number]
+
+# Packet parser
+def parser(packet):
+	dict = {}
+	layers = ""
+	# Agregation of all the packet's layers
+	for i in enumeration(packet):
 		if i == 'Raw':
 			break
-		else:
-			print(i)
-			diccionario = packet.getlayer(i).fields
-			print(diccionario)
+		if (layers == ""):
+			layers = i
+		layers += ',' + i
+	dict['layers'] = layers
+	# Agregation of specific packet's data
+	try:
+		dict['Mac-origen'] = getattr(packet.getlayer('Ethernet'),'src')
+		dict['IP-origen'] = getattr(packet.getlayer('IP'),'src')
+		dict['IP-destino'] = getattr(packet.getlayer('IP'),'dst')
+		if(packet.getlayer('TCP')):
+			dict['TCP-seq'] = getattr(packet.getlayer('TCP'),'seq')
+			dict['Puerto-origen'] = getattr(packet.getlayer('TCP'),'sport')
+			dict['Puerto-destino'] = getattr(packet.getlayer('TCP'),'dport')
+		if(packet.getlayer('UDP')):
+			dict['Puerto-origen'] = getattr(packet.getlayer('UDP'),'sport')
+			dict['Puerto-destino'] = getattr(packet.getlayer('UDP'),'dport')
 
-scapy.sniff(filter='ip',prn=printer)
+	except Exception:
+		print('err')
+	# Json creation and data sending
+	json_data = json.dumps(dict)
+	print(json_data)
+
+# Sniffer
+scapy.sniff(filter='ip',prn=parser)
