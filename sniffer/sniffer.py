142efd76b6ff9e4f65b0ce1787163567212ea20b
Guille <cartogreen@gmail.com>
2018-11-29 19:27:58 +0100

    Post request - sniffer

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -3,6 +3,7 @@ import scapy.all as scapy
 import socket
 import json
 import time
+import requests
 
 # Enumeration of the packet's layers
 def enumeration(packet):
@@ -11,18 +12,14 @@ def enumeration(packet):
 		packet =packet.payload
 		yield packet.name
 
-# # Mapping between the protocol number and its name
-# def protocol(number):
-# 	table = {num:name[8:] for name,num in vars(socket).items() if name.startswith("IPPROTO")}
-# 	return table[number]
-
-# Packet parser
+# Epoch milliseconds calculator
 epoch_millis = lambda: int(round(time.time() * 1000))
 
+# Packet parser
 def parser(packet):
-	dict = {}
+	dictionary = {}
 	# Timestamp identification for Grafana and ElasticSearch
-	dict['@timestamp'] = epoch_millis()
+	dictionary['@timestamp'] = epoch_millis()
 	layers = ""
 	# Agregation of all the packet's layers
 	for i in enumeration(packet):
@@ -31,25 +28,30 @@ def parser(packet):
 		if (layers == ""):
 			layers = i
 		layers += ',' + i
-	dict['layers'] = layers
+	dictionary['layers'] = layers
 	# Agregation of specific packet's data
 	try:
-		dict['MacOrigen'] = getattr(packet.getlayer('Ethernet'),'src')
-		dict['IpOrigen'] = getattr(packet.getlayer('IP'),'src')
-		dict['IpDestino'] = getattr(packet.getlayer('IP'),'dst')
+		dictionary['MacOrigen'] = getattr(packet.getlayer('Ethernet'),'src')
+		dictionary['IpOrigen'] = getattr(packet.getlayer('IP'),'src')
+		dictionary['IpDestino'] = getattr(packet.getlayer('IP'),'dst')
 		if(packet.getlayer('TCP')):
-			dict['TCPseq'] = getattr(packet.getlayer('TCP'),'seq')
-			dict['PuertoOrigen'] = getattr(packet.getlayer('TCP'),'sport')
-			dict['PuertoDestino'] = getattr(packet.getlayer('TCP'),'dport')
+			dictionary['TCPseq'] = getattr(packet.getlayer('TCP'),'seq')
+			dictionary['PuertoOrigen'] = getattr(packet.getlayer('TCP'),'sport')
+			dictionary['PuertoDestino'] = getattr(packet.getlayer('TCP'),'dport')
 		if(packet.getlayer('UDP')):
-			dict['PuertoOrigen'] = getattr(packet.getlayer('UDP'),'sport')
-			dict['PuertoDestino'] = getattr(packet.getlayer('UDP'),'dport')
+			dictionary['PuertoOrigen'] = getattr(packet.getlayer('UDP'),'sport')
+			dictionary['PuertoDestino'] = getattr(packet.getlayer('UDP'),'dport')
 
 	except Exception:
 		print('err')
 	# Json creation and data sending
-	json_data = json.dumps(dict)
+	json_data = json.dumps(dictionary)
 	print(json_data)
+	try:
+		resp = requests.post("http://localhost:5001/save", data=json_data)
+		print(resp.status_code, resp.reason)
+	except Exception:
+		print('error de conexion')
 
 # Sniffer
 scapy.sniff(filter='ip',prn=parser)
