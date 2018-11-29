7bc4e5013a75d6448db049c259ac7c99d6bea37a
David Ramírez <el.yerros@gmail.com>
2018-11-29 19:22:40 +0100

    api v 0.1

--- local_server/main.py
+++ local_server/main.py
@@ -1,35 +1,40 @@
 from datetime import datetime
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from elasticsearch import Elasticsearch
 
-
-'''
-	API REST Server 
-'''
+"""
+===================================
+API REST- Local Server
+===================================
+--> Processes requests from IoT Sniffer and feeds Elasticsearch
+--> Todo: Provides authentication and session control
+--> 
+"""
 
 __author__ = "IoT Vigilant"
 
+# Params
 es_host = "localhost"
 es_port = 9200
 es_index = "rawdata"
 es_doc_type = "ts"
 
+# Elasticsearch instance
 es = Elasticsearch([{'host': es_host, 'port': es_port}])
-
+# Flask instance
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def index():
-    results = es.indices.get_alias("*")
-    return jsonify(results)
-
-@app.route('/', methods=['POST'])
-def index():
-     result = es.index(index=es_index, doc_type='title', id=slug, body=body)
-    return jsonify(results)
-
-
+# Receive packets data from sniffer
+@app.route('/save', methods=['POST'])
+def index_post():
+	if request.json: # if request body is json
+		mydata = request.json
+		# feeds 
+		result = es.index(index=es_index, doc_type=es_doc_type, body=mydata)
+		print(result)
+	
+	return Response(result, status=201, mimetype='application/json')
 
 
 app.run(port=5001, debug=True)
