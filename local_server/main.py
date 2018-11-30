37145399d5957663b5f07321aa3f6963fb14c49a
Carlospolop <carlospolop@gmail.com>
2018-11-30 16:59:42 +0100

    change server_main.py

--- local_server/main.py
+++ local_server/main.py
@@ -1,24 +1,55 @@
+#!/usr/bin/python3
+
+import time, argparse 
+
 from datetime import datetime
 from flask import Flask, jsonify, request, Response
 from elasticsearch import Elasticsearch
-import time
+
 
 """
 ===================================
 API REST- Local Server
 ===================================
---> Processes requests from IoT Sniffer and feeds Elasticsearch
+Get information from IoTVigilant sniffers and feed Elasticsearch with it
 --> Todo: Provides authentication and session control
---> 
 """
-
-__author__ = "IoT Vigilant"
-
-# Params
+listen_port = 5001
 es_host = "localhost"
 es_port = 9200
 es_index = "rawdata"
 es_doc_type = "ts"
+
+def config_params():
+	"""
+	Configure the parameters used to connect to the Elasticsearch and the port where the server is going to listen
+	"""
+	global listen_port, es_host, es_port
+	
+	arg_parser = argparse.ArgumentParser()
+	arg_parser.add_argument('--elasticsearch-host', action='store',
+						dest='es_host',
+						default='localhost')
+	arg_parser.add_argument('--elasticsearch-port', action='store',
+						dest='es_port',
+						default='9200', type=int)
+	arg_parser.add_argument('--listen-port', action='store',
+						dest='listen_port',
+						default=5001, type=int)
+	parameters = arg_parser.parse_args()
+
+	if "IOTV_ES_HOST" in os.environ:
+		parameters.es_host = os.getenv("IOTV_ES_HOST")
+	if "IOTV_ES_PORT" in os.environ:
+		parameters.es_port = os.getenv("IOTV_ES_PORT")
+	if "IOTV_LISTEN_PORT" in os.environ:
+		parameters.listen_port = os.getenv("IOTV_LISTEN_PORT")
+	
+	es_host = parameters.es_host
+	es_port = parameters.es_port
+	listen_port = parameters.listen_port
+
+config_params()
 
 # Elasticsearch instance
 es = Elasticsearch([{'host': es_host, 'port': es_port}])
@@ -38,4 +69,4 @@ def index_post():
 		return Response("{ }", status=201, mimetype='application/json')
 	return Response("{ }", status=400, mimetype='application/json')
 
-app.run(host='0.0.0.0', port=5001, debug=True)
+app.run(host='0.0.0.0', port=listen_port, debug=True)
