969f9fbf74f51d7b53659b9690104ba7b18bb29d
Carlos <carlospolop@gmail.com>
2018-12-01 10:01:16 +0100

    entryfile docker

--- docker/local_server/netflow/run.sh
+++ docker/local_server/netflow/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nfcapd -p 2055 -b 0.0.0.0 -l /tmp/netflowpckts -D -x "python3 /root/iotvigilant/local_server/netflow/parse_netflow.py /tmp/netflowpckts"
+nfcapd -p 2055 -b 0.0.0.0 -l /tmp/netflowpckts -x "python3 /root/iotvigilant/local_server/netflow/parse_netflow.py /tmp/netflowpckts"
